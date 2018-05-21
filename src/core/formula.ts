97dddc28007c5328ee7ffa7645574808e08b7c0e
myliang <liangyuliang0335@126.com>
2018-05-21 20:33:42 +0800

    formalu cal support ..=sum(50, 24 * (10-5), B12)

--- src/core/formula.ts
+++ src/core/formula.ts
@@ -11,7 +11,7 @@ export const formulaFilterKey = (v: string, filter: (formula: Formula, param: st
     const fx = v.substring(1, v.indexOf('('))
     for (let formula of formulas) {
       if (formula.key.toLowerCase() === fx.toLowerCase()) {
-        return filter(formula, v.substring(v.indexOf('(') + 1, v.indexOf(')')))
+        return filter(formula, v.substring(v.indexOf('(') + 1, v.lastIndexOf(')')))
       }
     }
   }
@@ -27,10 +27,13 @@ export const formulaRender = (v: string, renderCell: (rindex: number, cindex: nu
 export const formulaReplaceParam = (param: string, rowDiff: number, colDiff: number): string => {
   return formulaFilterKey(param, (fx, params) => {
     const replaceFormula = (_v: string):string => {
+      if (/^[0-9\-\+\*\/()\s]+$/.test(_v.trim())) {
+        return _v
+      }
       const idx = /\d+/.exec(_v)
       if (idx) {
-        let vc = _v.substring(0, idx.index)
-        let vr = parseInt(_v.substring(idx.index))
+        let vc = _v.substring(0, idx.index).trim()
+        let vr = parseInt(_v.substring(idx.index).trim())
         return `${alphabet(alphabetIndex(vc) + colDiff)}${vr + rowDiff}`
       }
       return _v;
@@ -55,11 +58,11 @@ const formulaParamToArray = (param: string, renderCell: (rindex: number, cindex:
       if (idx && maxIdx) {
         // idx = idx.index;
         // maxIdx = maxIdx.index;
-        let minC = min.substring(0, idx.index)
-        let minR = parseInt(min.substring(idx.index))
+        let minC = min.substring(0, idx.index).trim()
+        let minR = parseInt(min.substring(idx.index).trim())
 
-        let maxC = max.substring(0, maxIdx.index)
-        let maxR = parseInt(max.substring(maxIdx.index))
+        let maxC = max.substring(0, maxIdx.index).trim()
+        let maxR = parseInt(max.substring(maxIdx.index).trim())
         // console.log(min, max, minR, maxR, minC, maxC)
         if (maxC === minC) {
           for (let i = minR; i <= maxR; i++) {
@@ -72,14 +75,23 @@ const formulaParamToArray = (param: string, renderCell: (rindex: number, cindex:
           }
         }
       }
-    } else if (param.indexOf(',') !== -1) {
+    } else {
       paramValues = param.split(',').map(p => {
+        // console.log(/^[0-9\-\+\*\/() ]+$/.test(p), p)
+        if (/^[0-9\-\+\*\/()\s]+$/.test(p.trim())) {
+          try {
+            return eval(p)
+          } catch (e) {
+            return 0
+          }
+        }
         const idx = /\d+/.exec(p)
         if (idx) {
-          const c = p.substring(0, idx.index)
-          const r = p.substring(idx.index)
+          const c = p.substring(0, idx.index).trim()
+          const r = p.substring(idx.index).trim()
           return renderCell(parseInt(r) - 1, alphabetIndex(c))
         }
+        return 0
       })
     }
   } catch (e) {
